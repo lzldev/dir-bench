82014152406171ff3521aa048e337d55f8f9b419
Lucas Costa <francisco.lucas0507@gmail.com>
2024-12-22 10:10:10 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# bench-test
+
+bench-test provides a macro to generate single test cases from files in a directory.
+
+based on [dir-test](https://crates.io/crates/dir-test)
---
+++ bench-test/src/lib.rs
@@ -0,0 +1,29 @@
+pub struct Fixture<T> {
+    content: T,
+    path: &'static str,
+}
+
+impl<T> Fixture<T> {
+    #[doc(hidden)]
+    /// Creates a new fixture from the given content and path.
+    pub fn new(content: T, path: &'static str) -> Self {
+        Self { content, path }
+    }
+
+    /// Returns a reference to the content of the fixture.
+    pub fn content(&self) -> &T {
+        &self.content
+    }
+
+    /// Consumes the fixture and returns the content.
+    pub fn into_content(self) -> T {
+        self.content
+    }
+
+    /// Returns the absolute path of the fixture.
+    pub const fn path(&self) -> &'static str {
+        self.path
+    }
+}
+
+pub use bench_test_macros::*;
